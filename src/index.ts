e53dc13ff6082a9c8307030176ca7b5a594568e5
Daniel Mathisen Breivik <danielmbreivik@hotmail.com>
2023-02-04 02:23:10 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import generateJakeRachelMike from 'lib/plays/JakeRachelMike';
 import actPlay, { setDebug } from './tts';
 import * as fs from 'fs/promises';
-import speak, { listVoices } from 'lib/speech';
 setDebug(true);
 (async () => {
   let { play, characters } = await generateJakeRachelMike();
